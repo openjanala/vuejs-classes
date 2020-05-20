83d5445e1f366e0043293c034b281022e6baae0e
openjanala <openjanala@gmail.com>
2020-05-20 11:16:28 +0600

    class 19 completed of axios request

--- src/components/Hoke.vue
+++ src/components/Hoke.vue
@@ -1,40 +1,47 @@
 <template>
-        <div class="Hoke">
-            <h2>{{title}}</h2>
-        <button @click="changeTitle">Update</button>
+        <div class="post">
+            <h1>Posts</h1><hr>
+            <div v-for="(post) in posts" :key="post.id">
+                <h2>{{post.id}}. {{post.title}}</h2>
+                <p>{{post.body}}</p><hr>
+            </div>
         </div>
 </template>
 
 
 <script>
+import axios from 'axios'
 export default {
    name:'Hoke',
    data(){
        return{
-        title:'Hoke Part'
+       posts:[]
        }
    },
    methods:{
-       changeTitle(){
-           this.title = 'openjanala IT'
-       }
+       
    },
-    beforeCreate(){
-//alert('beforeCreate')
-    },
     created(){
-//alert('created')
-    },
-    beforeUpdate(){
-        alert('befor update')
-    },
-    updated(){
-        alert('updated')
-    },
+        axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then(response =>{
+            //console.log(response)
+            this.posts = response.data
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
 }
 </script>
 
 
 <style scoped>
-
+h1{
+    color:red;
+    text-align: center;
+}
+.post{
+text-align: left;
+}
 </style>
